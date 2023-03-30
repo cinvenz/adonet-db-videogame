89e18149a499b77d932b0b36cc9ea613cf9832bd
cinvenz <vincenzobortune@gmail.com>
2023-03-30 17:14:43 +0200

    fix

--- Program.cs
+++ Program.cs
@@ -15,33 +15,4 @@ Console.WriteLine("Ricerca gioco per ID");
 var userId = Convert.ToInt64(Console.ReadLine());
 
 
-var videogame = connessioneSql.GetVideogameByIdLike(userId);
-
-foreach (var item in videogame) Console.WriteLine(item);
-
-Console.WriteLine("Inserisci un videogioco");
-
-Console.WriteLine("Aggiungi un nome");
-var gameName = Console.ReadLine();
-
-Console.WriteLine("Aggiungi una overview");
-var gameOverview = Console.ReadLine();
-
-Console.WriteLine("Aggiungi una data di rilascio");
-var gameRelease = Convert.ToDateTime(Console.ReadLine());
-
-Console.WriteLine("Aggiungi software house ID");
-var gameSoftwareHouse = Convert.ToInt64(Console.ReadLine());
-
-videogameManager.AddVideoGame(gameName, gameOverview, gameRelease, gameSoftwareHouse);
-
-Console.WriteLine("Ricerca per nome");
-var userName = Console.ReadLine();
-
-
-var videogameName = videogameManager.GetVideoGameByNameLike(userName);
-
-
-foreach (var item in videogameName) Console.WriteLine(item);
-
-Console.WriteLine($"Count: {videogameName.Count}");
+v
